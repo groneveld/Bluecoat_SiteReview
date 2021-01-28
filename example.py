709026573fc40a9f529a046f0893856b94a43ad9
Mariya Strogova <groneveld.co@gmail.com>
2021-01-28 22:23:17 +0700

    Simple example added

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
-from SiteReview import SiteReview
-import os, inspect
-import fnmatch
+from SiteReview import SiteReview, Wrong_TLD
 
 categorizer = SiteReview()
-
-print(categorizer.get_category('vk.uu'))
+domain_names = ['google.com', 'youtube.com', 'dom.uu']
+for domain in domain_names:
+    try:
+        cat = categorizer.get_category(domain, names_only=True)
+        print(domain, cat)
+    except Wrong_TLD:
+        print(f'Wrong domain for {domain}')
+        pass
