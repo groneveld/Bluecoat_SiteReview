634b788b1f150346d90e098358b1ef9006c81537
Mariya Strogova <groneveld.co@gmail.com>
2021-01-28 22:41:32 +0700

    Simple explanations added

--- SiteReview.py
+++ SiteReview.py
@@ -10,12 +10,15 @@ import requests
 from PIL import Image
 
 
-class Wrong_TLD(Exception):
+# defining an exception for wrong top-level domain error
+class WrongTLD(Exception):
     def __init__(self, message="Wrong Top-Level Domain"):
         self.message = message
         super().__init__(self.message)
 
+
 class SiteReview:
+    # headers for requests
     headers = {
         'authority': 'sitereview.bluecoat.com',
         'accept': 'application/json, text/plain, */*',
@@ -31,6 +34,7 @@ class SiteReview:
         'cookie': 'JSESSIONID=FD00E4699288139952D7B9CF25DC6F61; XSRF-TOKEN=ce6e0505-ed4b-43ef-9624-9a5ad3b70f27',
     }
 
+    # special headers for reCaptcha
     _headers = {
         'authority': 'sitereview.bluecoat.com',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
@@ -43,7 +47,8 @@ class SiteReview:
         'cookie': 'JSESSIONID=FD00E4699288139952D7B9CF25DC6F61; XSRF-TOKEN=ce6e0505-ed4b-43ef-9624-9a5ad3b70f27; __utma=265713933.443691065.1605382266.1605382266.1605382266.1; __utmc=265713933; __utmz=265713933.1605382266.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
     }
 
-    def get_captcha_base64(self):
+    # get decoded captcha answer
+    def __get_captcha_base64(self):
         epoch_timestamp = str(calendar.timegm(time.gmtime()) * 1000)
         captcha_url = 'https://sitereview.bluecoat.com/resource/captcha.jpg?%s' % (epoch_timestamp)
         local_filename = 'captcha.jpg'
@@ -64,37 +69,42 @@ class SiteReview:
         base64_captcha = base64.b64encode(captcha_utf)
         return base64_captcha.decode("utf-8")
 
-    def get_data(self, url: str):
+    # create data for request with specific URL
+    def __get_data(self, url: str):
         return json.dumps(
             {"url": url, "captcha": "", "key": "f69e0635f42170328ee88fac4276ffb17a587931f2cd31ad0bdd2759005e0cc2",
              "phrase": "U2NyaXB0aW5nIGFnYWluc3QgU2l0ZSBSZXZpZXcgaXMgYWdhaW5zdCB0aGUgU2l0ZSBSZXZpZXcgVGVybXMgb2YgU2VydmljZQ==",
              "source": ""})
 
-    def captcha_recognize(self):
-        captcha = self.get_captcha_base64()
+    # send captcha answer
+    def __captcha_recognize(self):
+        captcha = self.__get_captcha_base64()
         response = requests.get(f'https://sitereview.bluecoat.com/resource/captcha-request/{captcha}',
                                 headers=self._headers)
 
-    def get_names_only(self, categories):
+    # if names_only == True -> get only category names from answer dictionaries
+    def __get_names_only(self, categories):
         answers = []
         for answer in categories:
             category_name = answer['name']
             answers.append(category_name[category_name.find("(") + 1:category_name.find(")")])
         return answers
 
-    def get_category(self, url: str, names_only=False, is_again=False) -> str:
-        data = self.get_data(url)
+    def get_category(self, url: str, names_only=False, is_again=False) -> list:
+        data = self.__get_data(url)
         response = requests.post('https://sitereview.bluecoat.com/resource/lookup', headers=self.headers, data=data)
         if response.status_code == 200:
             if names_only:
-                return self.get_names_only(json.loads(response.text)['categorization'])
+                return self.__get_names_only(json.loads(response.text)['categorization'])
             return json.loads(response.text)['categorization']
         if response.status_code == 422:
-            raise Wrong_TLD
+            raise WrongTLD
+        # this condition is required for every 10th request
         elif not is_again:
-            self.captcha_recognize()
-            return self.get_category(url, True)
+            self.__captcha_recognize()
+            return self.get_category(url, is_again=True)
+        # this condition is required when user's IP is temporarily blocked by SiteReview
         else:
             time.sleep(5)
-            self.captcha_recognize()
-            return self.get_category(url, True)
+            self.__captcha_recognize()
+            return self.get_category(url, is_again=True)
--- example.py
+++ example.py
@@ -1,11 +1,11 @@
-from SiteReview import SiteReview, Wrong_TLD
+from SiteReview import SiteReview, WrongTLD
 
 categorizer = SiteReview()
-domain_names = ['google.com', 'youtube.com', 'dom.uu']
+domain_names = ['google.com', 'youtube.com', 'dom.uu', 'facebook.com']
 for domain in domain_names:
     try:
         cat = categorizer.get_category(domain, names_only=True)
         print(domain, cat)
-    except Wrong_TLD:
+    except WrongTLD:
         print(f'Wrong domain for {domain}')
         pass
